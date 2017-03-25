bca88ffb9b0785e15f76402ea7d55d121e2bf1be
Chang Hao Chao <changhaochao@Changde-MacBook-Pro.local>
2017-03-25 15:04:04 +0800

    v1.0

---
+++ Matchismo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Matchismo
+//
+//  Created by Chang Hao Chao on 2017/3/25.
+//  Copyright © 2017年 Chang Hao Chao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Matchismo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Matchismo
+//
+//  Created by Chang Hao Chao on 2017/3/25.
+//  Copyright © 2017年 Chang Hao Chao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
