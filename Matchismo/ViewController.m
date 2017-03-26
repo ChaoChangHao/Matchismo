3798a87d6b0f6d8288d0d1b135538a1efeedd6b5
Chang Hao Chao <changhaochao@Changde-MacBook-Pro.local>
2017-03-26 20:16:52 +0800

    V1.1

--- Matchismo/ViewController.m
+++ Matchismo/ViewController.m
@@ -7,14 +7,54 @@
 //
 
 #import "ViewController.h"
+#import "PlayingCardDeck.h"
+#import "CardMatchingGame.h"
 
 @interface ViewController ()
-
+@property (strong, nonatomic) CardMatchingGame *game;
+@property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *cardButtons;
+@property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
 @end
 
 @implementation ViewController
 
+- (CardMatchingGame *)game
+{
+    if(!_game) _game = [[CardMatchingGame alloc] initWithCardCount:[[self cardButtons] count]
+                                                         usingDeck:[self createDeck]];
+    return _game;
+}
 
+- (Deck *)createDeck
+{
+    return [[PlayingCardDeck alloc] init];
+}
 
+- (IBAction)touchCardButton:(UIButton *)sender {
+    NSUInteger chosenButtonIndex = [self.cardButtons indexOfObject:sender];
+    [self.game chooseCardAtIndex:chosenButtonIndex];
+    [self updateUI];
+}
+
+- (void)updateUI
+{
+    for (UIButton *cardButton in self.cardButtons) {
+        NSUInteger cardButtonIndex = [self.cardButtons indexOfObject:cardButton];
+        Card *card = [self.game cardAtIndex:cardButtonIndex];
+        [cardButton setTitle:[self titleForCard:card] forState:UIControlStateNormal];
+        [cardButton setBackgroundImage:[self titleForCardImage:card] forState:UIControlStateNormal];
+        cardButton.enabled = !card.isMatched;
+        self.scoreLabel.text = [NSString stringWithFormat:@"Score : %ld", (long)self.game.score];
+    }
+}
+
+- (NSString *)titleForCard:(Card *)card
+{
+    return card.isChosen ? card.contents : @"";
+}
+- (UIImage *)titleForCardImage:(Card *)card
+{
+    return [UIImage imageNamed:card.isChosen ? @"card_blank" : @"card_back"];
+}
 
 @end
